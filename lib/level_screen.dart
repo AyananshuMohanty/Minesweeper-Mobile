ac1a0b4df51ed36b6577b16282192507e96ae61b
Ayananshu Mohanty <119516509+AyananshuMohanty@users.noreply.github.com>
2024-02-17 03:37:44 +0530

    Added Dropdown Menu
    Added dropdown menu to select the number of sides of box in Minesweeper

--- lib/level_screen.dart
+++ lib/level_screen.dart
@@ -10,22 +10,49 @@ class LevelScreen extends StatefulWidget {
 }
 
 class _LevelScreenState extends State<LevelScreen> {
+  final TextEditingController numberOfSidesController = TextEditingController();
+  int? selectedNumberOfSides;
   @override
   Widget build(context) {
-    return const Center(
+    return Center(
         child: Column(
       mainAxisSize: MainAxisSize.min,
       children: [
         Expanded(
-            child: Center(
-                child: Text(
-          "Choose number of sides",
-          style: TextStyle(
-            color: Color.fromARGB(255, 34, 40, 49),
-            fontSize: 24,
+          child: Center(
+            child: Text(
+              "Choose number of sides",
+              style: TextStyle(
+                color: Color.fromARGB(255, 34, 40, 49),
+                fontSize: 24,
+              ),
+            ),
           ),
-        ))),
-        SizedBox(height: 500),
+        ),
+        DropdownMenu<int>(
+          initialSelection: 3,
+          controller: numberOfSidesController,
+          requestFocusOnTap: false,
+          label: const Text('Number of Sides'),
+          onSelected: (int? numberOfSides) {
+            setState(() {
+              selectedNumberOfSides = numberOfSides;
+            });
+          },
+          dropdownMenuEntries: [3, 4, 5, 6, 7, 8]
+              .map<DropdownMenuEntry<int>>((int numberOfSides) {
+            return DropdownMenuEntry<int>(
+              value: numberOfSides,
+              label: numberOfSides.toString(),
+              enabled: true,
+              style: MenuItemButton.styleFrom(
+                foregroundColor: Color.fromARGB(255, 34, 40, 49),
+              ),
+            );
+          }).toList(),
+          width: 300.0,
+        ),
+        SizedBox(height: 400),
         Align(
           alignment: Alignment.bottomCenter,
           child: Padding(
