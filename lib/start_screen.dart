4a19c33dabc685cfa1fe1bb35a78f22fe8c23a5b
Ayananshu Mohanty <119516509+AyananshuMohanty@users.noreply.github.com>
2024-02-20 23:38:30 +0530

    Short term fix for text at bottom

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -10,7 +10,9 @@ class StartScreen extends StatelessWidget {
         child: Column(
       mainAxisSize: MainAxisSize.min,
       children: [
-        Image.asset('assets/images/minesweeper.png', width: 300),
+        Expanded(
+          child: Image.asset('assets/images/minesweeper.png', width: 300),
+        ),
         const Text(
           "Minesweeper",
           style: TextStyle(
@@ -20,28 +22,32 @@ class StartScreen extends StatelessWidget {
         ),
         const SizedBox(height: 10),
         OutlinedButton.icon(
-            onPressed: levelSelector,
-            style: OutlinedButton.styleFrom(
-              foregroundColor: const Color.fromARGB(255, 34, 40, 49),
-            ),
-            icon: const Icon(Icons.play_arrow_rounded),
-            label: const Text(
-              "Start",
-              style: TextStyle(
-                color: Color.fromARGB(255, 34, 40, 49),
-                fontSize: 24,
-              ),
-            )),
-        const Align(
-          alignment: Alignment.bottomCenter,
-          child: Text(
-            "Ayananshu Mohanty",
+          onPressed: levelSelector,
+          style: OutlinedButton.styleFrom(
+            foregroundColor: const Color.fromARGB(255, 34, 40, 49),
+          ),
+          icon: const Icon(Icons.play_arrow_rounded),
+          label: const Text(
+            "Start",
             style: TextStyle(
-              color: Color.fromARGB(255, 251, 249, 241),
-              fontSize: 12,
+              color: Color.fromARGB(255, 34, 40, 49),
+              fontSize: 24,
             ),
           ),
         ),
+        SizedBox(height: 400),
+        const Align(
+            alignment: Alignment.bottomCenter,
+            child: Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Text(
+                "Ayananshu Mohanty",
+                style: TextStyle(
+                  color: Color.fromARGB(255, 251, 249, 241),
+                  fontSize: 12,
+                ),
+              ),
+            )),
       ],
     ));
   }
